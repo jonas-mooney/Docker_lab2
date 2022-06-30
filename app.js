87edc02230a752c91e9b3a59b64ae3077f037dcd
Jonas <jonasmooney2@gmail.com>
2022-06-30 09:39:41 -0600

    Initial commit lab2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const student = require('./router/student')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
+app.use('/homepage', express.static('public'))
+app.use(express.urlencoded({extended: false}));
 app.use('/student', student)
 
 app.get('/', (req, res) => {
